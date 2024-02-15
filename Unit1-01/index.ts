60526c209f2732fb1b1ae3484d2eb01c26e37efc
Samuel Webster <samuel.webster>
2024-02-15 09:44:45 -0500

    ts

---
+++ Unit1-01/index.ts
@@ -0,0 +1,32 @@
+/**
+
+ * The program shows how to get input
+
+ *   and deal with numbers.
+
+ *
+
+ * By:      Samuel Webster
+
+ * Version: 1.0
+
+ * Since:   2020-01-01
+
+ */
+
+
+
+import { createPrompt } from 'bun-promptx'
+
+console.log('Choose the length of the logs you would like to move with a truck.')
+const lengthString = createPrompt('Choose a maple log length from among 0.25m, 0.5m, and 1m: ')
+
+let lengthNumber = parseInt(lengthString.value)
+
+let logNumber = 55 / lengthNumber
+
+console.log(`The truck can carry ${logNumber} that are ${lengthNumber} long.`)
+
+
+
+console.log('\nDone.')
