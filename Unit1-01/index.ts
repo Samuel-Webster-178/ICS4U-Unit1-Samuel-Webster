f7eac5ab4632bdfd80d58db0f19b435c36640d34
Samuel-Webster-178 <99287154+Samuel-Webster-178@users.noreply.github.com>
2024-02-16 09:37:35 -0500

    Update index.ts

--- Unit1-01/index.ts
+++ Unit1-01/index.ts
@@ -15,16 +15,18 @@
  */
 
 
-
 import { createPrompt } from 'bun-promptx'
 
+//input
 console.log('Choose the length of the logs you would like to move with a truck.')
 const lengthString = createPrompt('Choose a maple log length from among 0.25m, 0.5m, and 1m: ')
 
+//process
 const lengthNumber = parseFloat(lengthString.value)
 
-const logNumber = 55 / lengthNumber
+const logNumber = (1100 / 20) / lengthNumber
 
+//output
 console.log(`The truck can carry ${logNumber} that are ${lengthNumber} long.`)
 
 
